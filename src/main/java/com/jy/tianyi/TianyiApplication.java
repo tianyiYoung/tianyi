7ab256b36bad9c1d3a66e082b8008a8e19a4f4c5
tianyi <gty55044@163.com>
2023-06-08 23:04:09 +0800

    init

---
+++ src/main/java/com/jy/tianyi/TianyiApplication.java
@@ -0,0 +1,13 @@
+package com.jy.tianyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianyiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TianyiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jy/tianyi/TianyiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jy.tianyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TianyiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
